e078db5cf31a5327dd705295926c2815f19155bc
Mike Kinney <mike.kinney@gmail.com>
2023-12-14 10:22:03 -0800

    got something working

--- main.go
+++ main.go
@@ -3,8 +3,53 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 	"os"
+	"strings"
+	"time"
 )
+
+func parseCheck(line string, timeout int) {
+	hostsAndPorts := strings.Split(line, " ")
+	//fmt.Println("hostsAndPorts:", hostsAndPorts)
+	allHosts := hostsAndPorts[0]
+	allPorts := hostsAndPorts[1]
+	//fmt.Println("allHosts:", allHosts)
+	//fmt.Println("allPorts:", allPorts)
+
+	hosts := strings.Split(allHosts, ",")
+	ports := strings.Split(allPorts, ",")
+	for _, host := range hosts {
+		//fmt.Println("host:", host)
+		for _, port := range ports {
+			checkHostAndPort(host, port, timeout)
+		}
+
+	}
+}
+
+func checkHostAndPort(host string, port string, timeout int) {
+	failures := 0
+	//fmt.Println("check host:", host, " port:", port)
+	address := net.JoinHostPort(host, port)
+	duration := time.Duration(int64(timeout) * int64(time.Second))
+	//fmt.Println("duration:", duration)
+	con, err := net.DialTimeout("tcp", address, duration)
+	if err != nil {
+		fmt.Println("FAILED host:", host, " port:", port)
+		failures++
+	} else {
+		if con != nil {
+			fmt.Println("SUCCESS host:", host, " port:", port)
+			con.Close()
+		} else {
+			fmt.Println("FAILED host:", host, " port:", port)
+			failures++
+		}
+
+	}
+	os.Exit(failures)
+}
 
 func main() {
 
@@ -19,8 +64,10 @@ func main() {
 		os.Exit(125)
 	}
 
-	fmt.Println("check:", *checkPtr)
-	fmt.Println("timeout:", *timeoutPtr)
+	//fmt.Println("check:", *checkPtr)
+	//fmt.Println("timeout:", *timeoutPtr)
+
+	parseCheck(*checkPtr, *timeoutPtr)
 
 	os.Exit(rc)
 }
